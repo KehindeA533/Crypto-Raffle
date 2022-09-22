dfab6b6a20475849b30baf4f96bb1050c00f0336
Kehinde. A <47582188+LegatoReign@users.noreply.github.com>
2022-09-21 20:01:15 -0400

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,33 +1,23 @@
 const { ethers } = require('hardhat')
 
 const networkConfig = {
-  // 4: {
-  //   name: 'rinkeby',
-  //   VRFCoordinatorV2: '0x6168499c0cffcacd319c818142124b7a15e857ab',
-  //   entranceFee: ethers.utils.parseEther('0.01'),
-  //   gasLane:
-  //     '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
-  //   subscriptionId: '10015',
-  //   callbackGasLimit: '500000', // 500,000
-  //   interval: '30', //30 seconds
-  // },
   31337: {
     name: 'hardhat',
     entranceFee: ethers.utils.parseEther('0.01'),
     gasLane:
-      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
+      '0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15',
     callbackGasLimit: '500000',
     interval: '30',
   },
-  4: {
-    name: 'rinkeby',
+  5: {
+    name: 'goerli',
     subscriptionId: '10015',
     gasLane:
-      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
+      '0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15', // 30 gwei aka Key hash
     interval: '30',
     entranceFee: ethers.utils.parseEther('0.01'), // 0.1 ETH
     callbackGasLimit: '500000', // 500,000 gas
-    vrfCoordinatorV2: '0x6168499c0cFfCaCD319c818142124B7A15E857ab',
+    vrfCoordinatorV2: '0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D',
   },
 }
 
